878cdaeaafa5f5cfd4507d6743e6a668872182fa
Wysciguwka <bartek14411@gmail.com>
2021-10-20 22:03:11 +0200

    Update z1.py

--- lista2/z1.py
+++ lista2/z1.py
@@ -1,13 +1,10 @@
 if __name__ == '__main__':
-    a = -5
-    b = 5
-    c = a + b
+    c = 7
     c += 1
     c -= 2
-    c *= 6 * c
+    c *= 6
     c //= 2
     c /= 3
     c %= 3
     c **= 2
-    # d = pow(c, 2)
-    print(a, b, c, d)
+    print(c)
