3e510cb5390784996137136c7607880dd56c7438
Wysciguwka <bartek14411@gmail.com>
2021-10-26 22:11:28 +0200

    Fix z3.py

--- lista3/z3.py
+++ lista3/z3.py
@@ -1,12 +1,17 @@
 def srednia_liczb(*args):
     """funckja obliczająca średnią N liczb zaokrągloną za pomocą funkcji int()"""
-    suma = 0
-    for arg in args:
-        suma += arg
+    if not args:
+        return "Nie podano liczb."
+    try:
+        suma = sum(args)
+        # można również użyć pętli for
+    except TypeError:
+        return "Podano niepoprawną liczbę jako argument."
     srednia = int(suma / len(args))
     parzystosc = f"parzysta" if srednia % 2 == 0 else f"nieparzysta"
     return f"Zaokrąglona średnia to: {srednia} i jest ona {parzystosc}"
 
 
 if __name__ == "__main__":
-    print(srednia_liczb(1, 2, 3, 4, 5, 6, 7, 8, 9))
+    # print(srednia_liczb.__doc__)
+    print(srednia_liczb(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
