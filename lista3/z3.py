6cf5ee432d31f43f22b66cb93e692af27ef5dc21
Wysciguwka <bartek14411@gmail.com>
2021-10-26 18:23:56 +0200

    Fix z3.py

--- lista3/z3.py
+++ lista3/z3.py
@@ -1,24 +1,12 @@
-def srednia_liczb():
+def srednia_liczb(*args):
     """funckja obliczająca średnią N liczb zaokrągloną za pomocą funkcji int()"""
-    try:
-        n = int(input("Z ilu liczb policzyć średnią: "))
-    except ValueError:
-        return "Podaj poprawną liczbę"
-    if n <= 0:
-        return "Ilość liczb musi być dodatnia"
     suma = 0
-    for i in range(n):
-        while True:  # while ValueError:
-            try:
-                x = float(input("liczba: "))
-                suma += x
-                break
-            except ValueError:
-                print("niepoprawna liczba")
-    srednia = int(suma / n)
+    for arg in args:
+        suma += arg
+    srednia = int(suma / len(args))
     parzystosc = f"parzysta" if srednia % 2 == 0 else f"nieparzysta"
-    return f"Średnia to: {srednia} i jest ona {parzystosc}"
+    return f"Zaokrąglona średnia to: {srednia} i jest ona {parzystosc}"
 
 
 if __name__ == "__main__":
-    print(srednia_liczb())
+    print(srednia_liczb(1,2,3,4,5,6,7,8,9))
