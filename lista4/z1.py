abb01ba787d8073acaa1bc5d3b7f74efbab7ea40
Wysciguwka <bartek14411@gmail.com>
2021-10-29 23:51:34 +0200

    Add l4/z1.py

---
+++ lista4/z1.py
@@ -0,0 +1,20 @@
+def shift(steps, input_list):
+    if not input_list:
+        return []
+    try:
+        if steps >= 0:
+            for i in range(0, steps):
+                input_list.insert(0, input_list.pop())  # usuwa ostatni element, wpisując go na miejsce 0
+        else:
+            for i in range(0, abs(steps)):
+                input_list.append(input_list.pop(0))  # usuwa pierwszy element, dodając go na koniec listy
+        return input_list
+    except TypeError:
+        return "Niepoprawna ilośc kroków"
+
+
+if __name__ == '__main__':
+    print(shift(3, []))
+    print(shift(3, [1, 2, 3, 4, 5, 6]))
+    print(shift(0, [1, 2, 3, 4, 5, 6]))
+    print(shift(-1, [1, 2, 3, 4, 5, 6]))
