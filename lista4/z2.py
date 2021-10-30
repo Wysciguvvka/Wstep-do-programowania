7fcb9e6fd0aeb4481da21bc0c94ce2be3d218be6
Wysciguwka <bartek14411@gmail.com>
2021-10-30 13:17:23 +0200

    Add lista4/z2.py

---
+++ lista4/z2.py
@@ -0,0 +1,13 @@
+def sortowanie(input_list):
+    try:
+        k_list = [x for x in sorted(input_list) if str(x)[0].upper() > 'K']
+        print(f'Nazwiska aczynające się od liter pózniejszych w alfabecie niz K: {", ".join(k_list).title()}')
+        s_list = [x for x in sorted(input_list) if len(x) > 5]
+        print(f'Nazwiska posortowanie alfabetycznie, zawierające >5 znaków: {", ".join(s_list).title()}')
+    except TypeError:
+        'podano niepoprawne nazwisko'
+
+
+if __name__ == '__main__':
+    nazwiska = ['Atestt', 'Ztest', 'Ktest', 'ltesttt', 'btest']
+    sortowanie(nazwiska)
