54c2cd8a14ec0ebf9149f8f665cda3682fd3ce47
Wysciguvvka <bartek14411@gmail.com>
2021-11-04 13:34:19 +0100

    Fix typo

--- lista4/z2.py
+++ lista4/z2.py
@@ -2,7 +2,7 @@ def sortowanie(input_list):
     """funkcja sortująca nazwiska i wypisująca te, które mają więcej niż 5 znaków"""
     try:
         k_list = [x for x in sorted(input_list) if str(x)[0].upper() > 'K']
-        print(f'Nazwiska aczynające się od liter pózniejszych w alfabecie niz K: {", ".join(k_list).title()}')
+        print(f'Nazwiska zaczynające się od liter pózniejszych w alfabecie niz K: {", ".join(k_list).title()}')
         s_list = [x for x in sorted(input_list) if len(x) > 5]
         print(f'Nazwiska posortowanie alfabetycznie, zawierające >5 znaków: {", ".join(s_list).title()}')
     except TypeError:
@@ -13,5 +13,5 @@ if __name__ == '__main__':
     nazwiska = ['Adamski', 'Zegar', 'Kowalski', 'limes', 'bogdan']
     sortowanie(nazwiska)
     print('---------')
-    nazwiska = [3, 'Zegar', 'Kowalski', 'limes', 'bogdan']
+    nazwiska = [3, 'Adamski', 'Kowalski', 'limes', 'bogdan']
     sortowanie(nazwiska)
