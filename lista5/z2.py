5f9fbd4484d4319b0e67345dee6695ecae082b0b
Wysciguvvka <bartek14411@gmail.com>
2021-11-16 12:48:02 +0100

    lista5

---
+++ lista5/z2.py
@@ -0,0 +1,19 @@
+class Prostokat:
+    """klasas wyliczająca pole prostokątku na podstawie długości boków"""
+
+    def __init__(self, a, b):
+        """init"""
+        self.a = a
+        self.b = b
+
+
+class Kwadrat:
+    """klasas wyliczająca pole kwadratu na podstawie długości boków"""
+
+    def __init__(self):
+        """init"""
+        pass
+
+
+if __name__ == "__main__":
+    pass
