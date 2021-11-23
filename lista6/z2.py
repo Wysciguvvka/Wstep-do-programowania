1c6034d20b464554878bfcb0f0b16cc831a6b88f
Wysciguwka <bartek14411@gmail.com>
2021-11-23 20:26:29 +0100

    Add class admin

--- lista6/z2.py
+++ lista6/z2.py
@@ -2,13 +2,12 @@ from hashlib import md5
 
 
 class Uzytkownik:
-    def __init__(self, username, fname, lname, email, password, bdate):
+    def __init__(self, username, fname, lname, email, password):
         self.username = username
         self.email = email
         self.fname = fname
         self.lname = lname
         self.__password = md5(password.encode()).hexdigest()  # prywatny atrybut
-        self.bdate = bdate
         self.proby_logowania = 0
 
     def opisz_uzytkownika(self):
@@ -16,6 +15,12 @@ class Uzytkownik:
 
     def pozdrow_uzytkownika(self):
         print(f'Witam {self.fname.title()} {self.lname.title()}, pozdrawiam')
+
+    def dodaj_probe_logowania(self):
+        self.proby_logowania += 1
+
+    def zresetuj_proby_logowania(self):
+        self.proby_logowania = 0
 
     def zaloguj(self, password):
         self.proby_logowania += 1
@@ -25,8 +30,23 @@ class Uzytkownik:
             print('podano niepoprawne hasło')
 
 
+class Admin(Uzytkownik):
+    def __init__(self, username, fname, lname, email, password):
+        super().__init__(username, fname, lname, email, password)
+        self.przywileje = Przywileje()
+
+
+class Przywileje:
+    def __init__(self):
+        self.przywileje = ['Może dodać post', 'Może usunąć post', 'Może zablokować użytkownika',
+                           'Może odblokować użytkownika']
+
+    def pokaz_przywileje(self):
+        print(f"Przywileje: {', '.join([przywilej for przywilej in self.przywileje])}")
+
+
 if __name__ == '__main__':
-    user1 = Uzytkownik('nazwa', 'imie', 'nazwisko', 'email', 'haslo', 'urodziny')
+    user1 = Uzytkownik('nazwa', 'imie', 'nazwisko', 'email', 'haslo')
     user1.pozdrow_uzytkownika()
     user1.opisz_uzytkownika()
     user1.zaloguj('haslo')
