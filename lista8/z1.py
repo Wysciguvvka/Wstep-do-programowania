b2bfabe7641888f175de5b937194276959bd2c56
Wysciguwka <bartek14411@gmail.com>
2021-12-08 10:16:00 +0100

    fix typo

--- lista8/z1.py
+++ lista8/z1.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     with open('zad1.txt', 'a',
-              encoding='utf-8') as file:  # parametr a tworzy plik, jeżeli plik nie istnieje, oraz zapisuje dane na koncu pliku
+              encoding='utf-8') as file:  # parametr a tworzy plik, jeżeli plik nie istnieje oraz zapisuje dane na koncu pliku
         while True:
             s = input('podaj tekst: ')
             if s == '': # s.replace(' ', '') żeby spacja liczyła się jako pusta linijka
