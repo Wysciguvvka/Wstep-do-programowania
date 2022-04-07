78f439d9559a0548b44443b1036c0c3680bbf130
Wysciguwka <bartek14411@gmail.com>
2022-04-07 21:33:40 +0200

    Fix2?

--- Zadanie/calendar.py
+++ Zadanie/calendar.py
@@ -10,12 +10,54 @@ class ListingStrategy:
     def begin(self) -> None:
         pass
 
-    @staticmethod
-    def event(title, date, time) -> None:
-        print(f'Title: {title}\nDate: {date}, {time}')
+    def event(self, title, date, time) -> None:
+        pass
 
     def end(self) -> None:
         pass
+
+
+class PrintStrategy(ListingStrategy):
+
+    def event(self, title, date, time) -> None:
+        print(f'Title: {title}\nDate: {date}, {time}')
+
+
+class ExportStrategy(ListingStrategy):
+    def __init__(self):
+        self.text = ''
+
+    def begin(self) -> None:
+        self.text = f'BEGIN:VCALENDAR\n' \
+                    f'VERSION:2.0\n' \
+                    f'BEGIN:VTIMEZONE\n' \
+                    f'TZID:Europe/Warsaw\n' \
+                    f'X-LIC-LOCATION:Europe/Warsaw\n' \
+                    f'END:VTIMEZONE'
+        print(f'BEGIN:VCALENDAR\n'
+              f'VERSION:2.0\n'
+              f'BEGIN:VTIMEZONE\n'
+              f'TZID:Europe/Warsaw\n'
+              f'X-LIC-LOCATION:Europe/Warsaw\n'
+              f'END:VTIMEZONE')
+
+    def event(self, title, date, time) -> None:
+        _date = date.split('.')
+        _time = time.replace(':', '')
+        _format = f'{_date[2]}{_date[1]}{_date[0]}T{_time}00'
+        _text = f'BEGIN:VEVENT\n' \
+                f'DTSTART:{_format}\n' \
+                f'DTEND:{_format}\n' \
+                f'SUMMARY:{title}\n' \
+                f'END:VEVENT'
+        self.text = f'{self.text}\n{_text}'
+        print(_text)
+
+    def end(self) -> None:
+        self.text = f'{self.text}\nEND:VCALENDAR'
+        print(f'END:VCALENDAR')
+        with open('calendar.ics', 'w+', encoding='utf8') as _ics:
+            _ics.write(self.text)
 
 
 def list_calendar(calendar: list[dict], listing_strategy) -> None:
@@ -23,6 +65,5 @@ def list_calendar(calendar: list[dict], listing_strategy) -> None:
 
     for event in calendar:
         listing_strategy.event(event['Title'], event['Date'], event['Time'])
-        pass
 
     listing_strategy.end()
--- Zadanie/menu.py
+++ Zadanie/menu.py
@@ -68,8 +68,8 @@ class ListCalendar(MenuCommand):
         return "List calendar"
 
     def execute(self) -> None:
-        from calendar import list_calendar, ListingStrategy
-        strategy = ListingStrategy()
+        from calendar import list_calendar, PrintStrategy
+        strategy = PrintStrategy()
         list_calendar(self.menu.calendar, strategy)
 
 
@@ -81,30 +81,10 @@ class ExportCalendar(MenuCommand):
         return "Export calendar to iCalendar"
 
     def execute(self) -> None:
-        _events = []
-        for event in self.menu.calendar:
-            _event = event.copy()
-            _date = _event['Date'].split('.')
-            _time = _event['Time'].replace(':', '')
-            _format = f'{_date[2]}{_date[1]}{_date[0]}T{_time}00'
-            _event_txt = f'BEGIN:VEVENT\n' \
-                         f'DTSTART:{_format}\n' \
-                         f'DTEND:{_format}\n' \
-                         f'SUMMARY:{_event["Title"]}\n' \
-                         f'END:VEVENT\n'
-            _events.append(_event_txt)
-        events = ''.join(_events)
-        _text = f'BEGIN:VCALENDAR\n' \
-                f'VERSION:2.0\n' \
-                f'BEGIN:VTIMEZONE\n' \
-                f'TZID:Europe/Warsaw\n' \
-                f'X-LIC-LOCATION:Europe/Warsaw\n' \
-                f'END:VTIMEZONE\n' \
-                f'{events or ""}' \
-                f'END:VCALENDAR'
-        print(_text)
-        with open('calendar.ics', 'w+', encoding='utf8') as _ics:
-            _ics.write(_text)
+        from calendar import list_calendar, ExportStrategy
+        strategy = ExportStrategy()
+        calendar = self.menu.calendar
+        list_calendar(calendar, strategy)
 
 
 class Menu:
